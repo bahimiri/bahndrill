5ed077a93389464c71f72d10b2caab5f21be24bc
Miriam Bah <miriam.bah@dkb.de>
2026-08-03 13:23:59 +0200

    chore(data): add zone information to data

--- src/stores/lineData.ts
+++ src/stores/lineData.ts
@@ -3,6 +3,14 @@ import { defineStore } from 'pinia'
 import type { Line, LineName, LineStop } from '@/types/lines.ts'
 import { isSBahn, isUBahn } from '@/utils/lineType.ts'
 
+type StopData = Record<
+  string,
+  {
+    lines: Array<string>
+    zone: string
+  }
+>
+
 export const useLineData = defineStore('lineData', () => {
   const initialized = ref(false)
   const lines = ref<Array<Line>>([])
@@ -21,10 +29,11 @@ export const useLineData = defineStore('lineData', () => {
     }
   }
 
-  function setStops(storageStops: Record<string, Array<string>>) {
+  function setStops(storageStops: StopData) {
     stops.value = Object.keys(storageStops).map((stopName: string) => ({
       name: stopName,
-      lines: storageStops[stopName] as Array<LineName>,
+      lines: storageStops[stopName]!.lines as Array<LineName>,
+      zone: storageStops[stopName]!.zone as 'A' | 'B' | 'C',
     }))
     if (lines.value.length && stops.value.length) {
       initialized.value = true
